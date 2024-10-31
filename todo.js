ddd8fd8fdd980fd3562652b2e0f6e2d1e9076344
Naufil (Z-xus) <naufilasar.dev@gmail.com>
2024-10-31 20:15:38 +0530

    feat(todo): add update todo option and improve list option

--- todo.js
+++ todo.js
@@ -39,10 +39,40 @@ async function createTask(description) {
   console.log(`Task created with ID: ${newTask.id}`);
 }
 
-async function listTasks() {
+async function listTasks(status) {
   const data = await readTasks();
+  if (!data.length) {
+    console.log("No tasks found");
+    return;
+  }
+
+  if (!["pending", "in-progress", "done", ""].includes(status)) {
+    throw new Error("Invalid status. Use 'pending', 'in-progress', 'done'");
+  }
+
+  const idWidth = 5;
+  const descWidth = 20;
+  const statusWidth = 15;
+
+  console.log(
+    `ID`.padEnd(idWidth) +
+      " | " +
+      `Description`.padEnd(descWidth) +
+      " | " +
+      `Status`.padEnd(statusWidth),
+  );
+  console.log("-".repeat(idWidth + descWidth + statusWidth + 6));
+
   for (t of data) {
-    console.log(`ID: ${t.id} - ${t.description}`);
+    if (status && t.status !== status) continue;
+
+    console.log(
+      `${t.id}`.padEnd(idWidth) +
+        " | " +
+        `${t.description}`.padEnd(descWidth) +
+        " | " +
+        `${t.status}`.padEnd(statusWidth),
+    );
   }
 }
 
@@ -56,6 +86,19 @@ async function deleteTask(id) {
   console.log(`Task ${id} deleted`);
 }
 
+async function updateTask(updateTaskId, newStatus) {
+  if (!["pending", "in-progress", "done"].includes(newStatus)) {
+    throw new Error("Invalid status. Use 'pending', 'in-progress', 'done'");
+  }
+  const data = await readTasks();
+  const task = data.find((t) => t.id === updateTaskId);
+  if (!task) throw new Error(`Task ${updateTaskId} not found`);
+  task.status = newStatus;
+  task.updatedAt = new Date();
+  await writeTasks(data);
+  console.log(`Task ${updateTaskId} updated to ${newStatus}`);
+}
+
 (async () => {
   const action = process.argv[2] || "help";
 
@@ -66,18 +109,25 @@ async function deleteTask(id) {
         await createTask(taskDesc);
         break;
       case "ls":
-        await listTasks();
+        const status = process.argv[3];
+        await listTasks(status);
         break;
       case "del":
         const taskId = process.argv[3];
         await deleteTask(parseInt(taskId));
         break;
+      case "update":
+        const updateTaskId = process.argv[3];
+        const newStatus = process.argv[4];
+        await updateTask(parseInt(updateTaskId), newStatus);
+        break;
       case "help":
         console.log(`
         Usage:
-        $ todo add "Task description"
-        $ todo ls
-        $ todo del taskId
+        $ todo add ["Task description"]
+        $ todo ls [status]
+        $ todo del [task ID]
+        $ todo update [task ID] [new status]
         `);
         break;
       default:
